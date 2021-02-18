dfbfbb155cad3e8ea76298e44da598ffbaafa35e
anshul bansal <akb.tech17@gmail.com>
2021-02-18 23:49:48 +0530

    craeted spans state, and updated it with the height of the img, after getting height it will change the image gridRowEnd style

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -8,11 +8,13 @@ import React from "react";
 class ImageCard extends React.Component {
   constructor(props) {
     super(props);
-
+    this.state = { spans: 0 };
     this.imageRef = React.createRef();
+    this.setSpan = this.setSpan.bind(this);
   }
 
   componentDidMount() {
+    
     //   //to find the height of the current img
     // console.log(this.imageRef.current.clientHeight);
     // //but this will console height as 0 // because at that moment image have not been rendered
@@ -20,12 +22,17 @@ class ImageCard extends React.Component {
   }
 
   setSpan = () => {
-    console.log(this.imageRef.current.clientHeight);
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 150);
+
+    this.setState({ spans });
+    // console.log(this.state.spans);
   };
   render() {
     const { description, urls } = this.props.image;
     return (
-      <div className="image-card">
+      <div className="image-card" style={{ gridRowEnd: `span ${this.state.spans}` }}>
         <img ref={this.imageRef} src={urls.regular} alt={description} />
       </div>
     );
