eabdfcb3fd8bcfd4aa4426277871268e5664500f
anshul bansal <akb.tech17@gmail.com>
2021-02-18 22:52:03 +0530

    wired up react-ref with all img tag being rendered by each image card, and console the ref object on screen

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,17 +1,25 @@
 import React from "react";
 
+//to wireup the react ref
+//create const
+//this.ref_name = react.createRef();
+//pass the ref prop with this.ref_name
+
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
 
-    
+    this.imageRef = React.createRef();
+  }
 
+  componentDidMount() {
+    console.log(this.imageRef);
+  }
   render() {
-    const {description,urls} = this.props.image;
+    const { description, urls } = this.props.image;
     return (
       <div className="image-card">
-        <img
-          src={urls.regular}
-          alt={description}
-        />
+        <img ref={this.imageRef} src={urls.regular} alt={description} />
       </div>
     );
   }
