c6e84077018239eb91f63155d19b8c8bb849cc3f
anshul bansal <akb.tech17@gmail.com>
2021-02-18 23:07:29 +0530

    fixed image height by setting up event listener on imageRef, when it gets load, one call-back function will invoke to get its height, and print it...

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -13,10 +13,15 @@ class ImageCard extends React.Component {
   }
 
   componentDidMount() {
-      //to find the height of the current img 
-    console.log(this.imageRef.current.clientHeight);
-    //but this will console height as 0 // because at that moment image have not been rendered
+    //   //to find the height of the current img
+    // console.log(this.imageRef.current.clientHeight);
+    // //but this will console height as 0 // because at that moment image have not been rendered
+    this.imageRef.current.addEventListener("load", this.setSpan);
   }
+
+  setSpan = () => {
+    console.log(this.imageRef.current.clientHeight);
+  };
   render() {
     const { description, urls } = this.props.image;
     return (
